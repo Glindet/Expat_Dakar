122cb65a46593f1c9282126dd2884168ec6bc834
Glindet <72458248+Glindet@users.noreply.github.com>
2025-02-10 21:36:58 +0000

    Update my_data_app.py

--- my_data_app.py
+++ my_data_app.py
@@ -1,3 +1,12 @@
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.google.com/"
+}
+
+res = requests.get(url, headers=headers)
+
+
 import streamlit as st
 import pandas as pd
 
